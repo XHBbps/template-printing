10e306a4ab28c3d22ffc83cefdde7866daf481e7
XHBbps <2674475871@qq.com>
2026-05-21 15:39:07 +0800

    feat(api): Prisma scaffold + initial User and RefreshToken models

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,8 +3,10 @@ import { LoggerModule } from 'nestjs-pino';
 
 // eslint-disable-next-line import/no-unresolved
 import { pinoConfig } from './common/logger.js';
+// eslint-disable-next-line import/no-unresolved
+import { PrismaModule } from './prisma/prisma.module.js';
 
 @Module({
-  imports: [LoggerModule.forRoot(pinoConfig(process.env.NODE_ENV ?? 'development'))],
+  imports: [LoggerModule.forRoot(pinoConfig(process.env.NODE_ENV ?? 'development')), PrismaModule],
 })
 export class AppModule {}
