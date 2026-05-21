6b7924db489162a3d106b1a5bb7f4fff0cda6d17
XHBbps <2674475871@qq.com>
2026-05-21 14:20:58 +0800

    chore: add ESLint + Prettier + husky + lint-staged

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'import/order': ['warn', { 'newlines-between': 'always', alphabetize: { order: 'asc' } }],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.config.cjs', '*.config.js', '.eslintrc.cjs'],
+};
