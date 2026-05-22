87c670bd3446030cd2bd7681e336b47b395887ab
XHBbps <2674475871@qq.com>
2026-05-22 17:22:41 +0800

    fix(renderer): text-bearing renderers add justifyContent so textAlign works in flex

--- packages/template-renderer/src/elements/SystemElement.vue
+++ packages/template-renderer/src/elements/SystemElement.vue
@@ -3,7 +3,7 @@ import { computed } from 'vue';
 
 // eslint-disable-next-line import/no-unresolved
 import type { TemplateElement } from '@template-printing/schema';
-import { styleToCss, verticalAlignToFlex } from '../styleToCss';
+import { styleToCss, verticalAlignToFlex, textAlignToJustify } from '../styleToCss';
 
 const props = defineProps<{
   element: Extract<TemplateElement, { type: 'system' }>;
@@ -36,6 +36,7 @@ const containerStyle = computed(() => ({
   ...styleToCss(props.element.style),
   display: 'flex',
   alignItems: verticalAlignToFlex(props.element.style.verticalAlign),
+  justifyContent: textAlignToJustify(props.element.style.textAlign),
   width: '100%',
   height: '100%',
   padding: `${props.element.style.padding.t}px ${props.element.style.padding.r}px ${props.element.style.padding.b}px ${props.element.style.padding.l}px`,
--- packages/template-renderer/src/elements/TextElement.vue
+++ packages/template-renderer/src/elements/TextElement.vue
@@ -2,7 +2,7 @@
 // eslint-disable-next-line import/no-unresolved
 import type { TemplateElement } from '@template-printing/schema';
 import { computed } from 'vue';
-import { styleToCss, verticalAlignToFlex } from '../styleToCss';
+import { styleToCss, verticalAlignToFlex, textAlignToJustify } from '../styleToCss';
 
 const props = defineProps<{
   element: Extract<TemplateElement, { type: 'text' }>;
@@ -13,6 +13,7 @@ const containerStyle = computed(() => ({
   ...styleToCss(props.element.style),
   display: 'flex',
   alignItems: verticalAlignToFlex(props.element.style.verticalAlign),
+  justifyContent: textAlignToJustify(props.element.style.textAlign),
   width: '100%',
   height: '100%',
   padding: `${props.element.style.padding.t}px ${props.element.style.padding.r}px ${props.element.style.padding.b}px ${props.element.style.padding.l}px`,
