1cd1c471cf4b15bde54f3e6992ff5cc0dbe0069c
XHBbps <2674475871@qq.com>
2026-05-22 17:24:37 +0800

    feat(renderer): ImageElement adopts styleToCss for border/bg/radius

--- packages/template-renderer/src/elements/ImageElement.vue
+++ packages/template-renderer/src/elements/ImageElement.vue
@@ -3,12 +3,25 @@ import { computed } from 'vue';
 
 // eslint-disable-next-line import/no-unresolved
 import type { TemplateElement } from '@template-printing/schema';
+import { styleToCss } from '../styleToCss';
 
 const props = defineProps<{
   element: Extract<TemplateElement, { type: 'image' }>;
   data?: Record<string, unknown>;
   designMode?: boolean;
 }>();
+
+const containerStyle = computed(() => ({
+  ...styleToCss(props.element.style),
+  width: '100%',
+  height: '100%',
+  boxSizing: 'border-box' as const,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  overflow: 'hidden',
+  padding: `${props.element.style.padding.t}px ${props.element.style.padding.r}px ${props.element.style.padding.b}px ${props.element.style.padding.l}px`,
+}));
 
 const src = computed<string | null>(() => {
   const s = props.element.source;
@@ -21,25 +34,13 @@ const objectFit = computed(() => props.element.fit ?? 'contain');
 </script>
 
 <template>
-  <div class="tp-image" :class="{ 'tp-image-design': props.designMode && !src }">
-    <img v-if="src" :src="src" :style="{ objectFit }" />
+  <div :style="containerStyle" :class="{ 'tp-image-design': props.designMode && !src }">
+    <img v-if="src" :src="src" :style="{ width: '100%', height: '100%', objectFit }" />
     <span v-else-if="props.designMode" class="tp-image-placeholder">▤ 图片</span>
   </div>
 </template>
 
 <style scoped>
-.tp-image {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-}
-.tp-image img {
-  width: 100%;
-  height: 100%;
-}
 .tp-image-design {
   border: 1px dashed #c0c7ff;
   background: linear-gradient(135deg, #fafafa, #eef1f4);
